f9ad0a6746a34ff6c173d7c55a979308e441575b
alessandrotraversi <alessandro.traversi@hotmail.it>
2016-02-26 21:13:44 -0300

    caricamento

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -67,7 +67,7 @@ Route::post('oauth/access_token', function() {
 
 Route::group(['prefix'=>'api', 'middleware'=>'oauth', 'as'=>'api.'], function () {
     Route::group(['prefix'=>'client', 'middleware'=>'oauth.checkrole:client', 'as'=>'client.'], function () {
-        Route::resource('order', 'Api/Client/ClientCheckoutController', 'except'=>['create','edit','destroy']);
+        Route::resource('/order', 'Api/Client/ClientCheckoutController', 'except'=>['create','edit','destroy']);
     });
 
     Route::group(['prefix'=>'deliveryman', 'middleware'=>'oauth.checkrole:deliveryman', 'as'=>'deliveryman.'], function () {
